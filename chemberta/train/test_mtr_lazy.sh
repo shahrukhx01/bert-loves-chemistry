69ac648e32ca5b218b87bcd403e0ab7e796c9376
shahrukhx01 <sk28671@gmail.com>
2022-09-17 23:33:20 +0200

    add bert for mtr

--- chemberta/train/test_mtr_lazy.sh
+++ chemberta/train/test_mtr_lazy.sh
@@ -2,8 +2,8 @@
 
 python train_roberta.py \
     --model_type=regression_lazy \
-    --dataset_path=../data/pubchem_1k_smiles.txt \
-    --normalization_path=../data/pubchem_descriptors_sample_1k_normalization_values_207.json \
+    --dataset_path=../data/guacamol_1k_smiles.txt \
+    --normalization_path=../data/guacamol_normalization_values_207.json \
     --output_dir=test_1k \
     --run_name=regression \
     --per_device_train_batch_size=8 \
